94529ba5a1b5f51dd069582e756c5e4e0d2811a7
Menno <mennomoes@hotmail.com>
2015-03-25 13:17:10 +0100

    Forms -> nieuwsitemscontroller update

--- WebCrawlerForms/WebCrawlerForms/NieuwsItemsController.cs
+++ WebCrawlerForms/WebCrawlerForms/NieuwsItemsController.cs
@@ -5,29 +5,28 @@ using System.Text;
 using System.Data;
 using System.Data.OleDb;
 using System.Data.Sql;
-using WebcrawlerService;
 
 namespace WebCrawlerForms
 {
-    public class NieuwsItemsController
+    public class NieuwsItemsController: IGenericObject
     {
-        public List<GenericObject> GetNieuwsItems()
+        public List<IGenericObject> GetNieuwsItems()
         { 
-            List<GenericObject> NieuwsItems = new List<GenericObject>();
+            List<IGenericObject> NieuwsItems = new List<IGenericObject>();
             DataTable dt = DAL.Select("SELECT * FROM Objecten WHERE Categorie = '1' ORDER BY Datum DESC, Tijd DESC");
 
             if (dt.Rows.Count > 0)
             {
                 for (int i = 0; i < dt.Rows.Count; i++)
                 {
-                    var test = new GenericObject();
-                    test.TitelProp = dt.Rows[i][1].ToString();
-                    test.BeschrijvingProp = dt.Rows[i][2].ToString();
-                    test.LinkProp = dt.Rows[i][3].ToString();
-                    test.MediaProp = dt.Rows[i][4].ToString();
-                    test.BronProp = dt.Rows[i][5].ToString();
-                    test.DatumProp = dt.Rows[i][6].ToString();
-                    NieuwsItems.Add(test);
+                    IGenericObject newObject = new NieuwsItemsController();
+                    newObject.GetTitel = dt.Rows[i][1].ToString();
+                    newObject.GetBeschrijving = dt.Rows[i][2].ToString();
+                    newObject.GetLink = dt.Rows[i][3].ToString();
+                    newObject.GetMedia = dt.Rows[i][4].ToString();
+                    newObject.GetBron = dt.Rows[i][5].ToString();
+                    newObject.GetDag = dt.Rows[i][6].ToString();
+                    NieuwsItems.Add(newObject);
                 }
             }
 
