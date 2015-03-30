a9da6d9e1515bd65a2b6c93f7a6b9710d0a27a59
boratnl <mennomoes@hotmail.com>
2015-03-30 12:02:07 +0200

    service app

---
+++ WebCrawlerForms/WebcrawlerServerService/WebcrawlerService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using WebcrawlerService;
+
+namespace WebcrawlerServerService
+{
+    class WebcrawlerService : System.ServiceProcess.ServiceBase
+    {
+        private Webcrawler crawler = new Webcrawler();
+        private System.Timers.Timer timer = new System.Timers.Timer();
+        // The main entry point for the process
+        static void Main()
+        {
+            System.ServiceProcess.ServiceBase[] ServicesToRun;
+            ServicesToRun =
+              new System.ServiceProcess.ServiceBase[] { new WebcrawlerService() };
+            System.ServiceProcess.ServiceBase.Run(ServicesToRun);
+        }
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ServiceName = "WinService";
+        }
+        private string folderPath = @"c:\temp";
+        /// <summary>
+        /// Set things in motion so your service can do its work.
+        /// </summary>
+        protected override void OnStart(string[] args)
+        {
+            if (!System.IO.Directory.Exists(folderPath))
+                System.IO.Directory.CreateDirectory(folderPath);
+
+            FileStream fs = new FileStream(folderPath + "\\WindowsService.txt",
+                                FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter m_streamWriter = new StreamWriter(fs);
+            m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+            m_streamWriter.WriteLine(" WindowsService: Service Started at " +
+               DateTime.Now.ToShortDateString() + " " +
+               DateTime.Now.ToShortTimeString() + "\n");
+            m_streamWriter.Flush();
+            m_streamWriter.Close();
+
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(OnTimedEvent);
+            timer.Interval = 60000;
+            timer.Enabled = true;
+        }
+
+        private void OnTimedEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            FileStream fs = new FileStream(folderPath + "\\WindowsService.txt",
+                                FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter m_streamWriter = new StreamWriter(fs);
+            m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+            m_streamWriter.WriteLine(" WindowsService: Data written started at " +
+               DateTime.Now.ToShortDateString() + " " +
+               DateTime.Now.ToShortTimeString() + "\n");
+            m_streamWriter.Flush();
+            m_streamWriter.Close();
+
+            crawler.CrawlContent();
+
+            m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+            m_streamWriter.WriteLine(" WindowsService: Data written finished at " +
+               DateTime.Now.ToShortDateString() + " " +
+               DateTime.Now.ToShortTimeString() + "\n");
+            m_streamWriter.Flush();
+            m_streamWriter.Close();
+        }
+
+        /// <summary>
+        /// Stop this service.
+        /// </summary>
+        protected override void OnStop()
+        {
+            timer.Enabled = false;
+            FileStream fs = new FileStream(folderPath +
+              "\\WindowsService.txt",
+              FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter m_streamWriter = new StreamWriter(fs);
+            m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+            m_streamWriter.WriteLine(" WindowsService: Service Stopped at " +
+              DateTime.Now.ToShortDateString() + " " +
+              DateTime.Now.ToShortTimeString() + "\n");
+            m_streamWriter.Flush();
+            m_streamWriter.Close();
+        }
+    }
+}
