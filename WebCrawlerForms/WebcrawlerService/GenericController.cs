ca97e072177c2c5e00561a38dc3dd1b098b9272e
boratnl <mennomoes@hotmail.com>
2015-04-02 14:58:37 +0200

    generic shit

---
+++ WebCrawlerForms/WebcrawlerService/GenericController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebcrawlerService
+{
+    public class GenericController: BaseCrawler
+    {
+        public void Crawl(string bron)
+        {
+            GenericModel model = new GenericModel(bron);
+            model.GetAllSources();
+        }
+    }
+}
